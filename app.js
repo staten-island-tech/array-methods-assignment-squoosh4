4357175ee1ee16aba564ab97305f10305b060a7e
squoosh4 <110200988+squoosh4@users.noreply.github.com>
2023-10-10 13:17:42 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,65 @@
 const rubicon_char {
 
-    
-}
+    {
+      name: "c4_621",
+      nickname: ["621", "Raven"],
+      species: "human(augmented)",
+      affiliation: "independent",
+      character_alignment_opinion: "true_neutral"}
+
+,
+    {
+      name: "cinder_carla",
+      nickname: "",
+      species: "",
+      affiliation: "",
+      character_alignment_opinion: ""
+
+},
+    {
+      name: "hander_walter",
+      nickname: "",
+      species: "",
+      affiliation: "",
+      character_alignment_opinion: ""
+
+},
+    {
+      name: "ayre",
+      nickname: "",
+      species: "",
+      affiliation: "",
+      character_alignment_opinion: ""
+
+},
+    {
+      name: "g1_michigan",
+      nickname: "",
+      species: "",
+      affiliation: "",
+      character_alignment_opinion: ""
+
+},
+    {
+      name: "g6_red",
+      nickname: "",
+      species: "",
+      affiliation: "",
+      character_alignment_opinion: ""
+
+},
+    {
+      name: "v_iv_rusty",
+      nickname: "",
+      species: "",
+      affiliation: "",
+      character_alignment_opinion: ""
+
+},
+    { 
+      name: "v_vii_swinburne",
+      nickname: "",
+      species: "",
+      affiliation: "",
+      character_alignment_opinion: ""
+}};
