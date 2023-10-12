741be3400eeb6ce8f5efd3c1408fc23b5da37c36
squoosh4 <110200988+squoosh4@users.noreply.github.com>
2023-10-12 13:16:52 -0400

    gersgser
    ergsgsergesr

--- app.js
+++ app.js
@@ -1,65 +1,65 @@
-const rubicon_char {
+const rubicon_char [
 
     {
       name: "c4_621",
       nickname: ["621", "Raven"],
-      ,
       affiliation: "independent",
-      character_alignment_opinion: "true_neutral"}
+      character_alignment_opinion: "true_neutral"
 
-,
+};
+
     {
       name: "carla",
       nickname: "cinder",
-      ,
       affiliation: "dosers",
       character_alignment_opinion: ""
 
-},
+};
+
     {
       name: "handler_walter",
       nickname: "n/a",
       ,
-      affiliation: "",
+      affiliation: "independent",
       character_alignment_opinion: ""
 
-},
+};
     {
       name: "ayre",
       nickname: "n/a",
       ,
-      affiliation: "",
-      character_alignment_opinion: ""
+      affiliation: "rubinconians",
+      character_alignment_opinion: "unknown"
 
-},
+};
     {
       name: "g1_michigan",
-      nickname: "",
+      nickname: "n/a",
       ,
-      affiliation: "",
-      character_alignment_opinion: ""
+      affiliation: "balam",
+      character_alignment_opinion: "chaotic_neutral"
 
-},
+};
     {
-      name: "g6_red",
-      nickname: "",
+      name: "g6_red";
+      nickname: "n/a",
       ,
-      affiliation: "",
-      character_alignment_opinion: ""
+      affiliation: "balam",
+      character_alignment_opinion: "neutral evil"
 
-},
+};
     {
       name: "v_iv_rusty",
-      nickname: "",
+      nickname: "n/a",
       ,
-      affiliation: "",
-      character_alignment_opinion: ""
+      affiliation: "arquebus",
+      character_alignment_opinion: "lawful neutral"
 
-},
+};
     { 
       name: "v_vii_swinburne",
-      nickname: "",
+      nickname: "n/a",
       ,
-      affiliation: "",
+      affiliation: "arquebus",
       character_alignment_opinion: ""
-}};
+}];
